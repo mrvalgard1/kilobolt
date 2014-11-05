000c51c0e981370d61845ed10594bcb677f585c7
valgard <valgard1@o2.pl>
2014-11-05 15:06:11 +0100

    1.1

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -3,23 +3,43 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
 
+
+	private Robot robot;
+
+	private Image image, character;
+	private Graphics second;
+	private URL base;
+	
 	@Override
 	public void init() {
 		setSize(800, 480);
 		setBackground(Color.BLACK);
 		setFocusable(true);
+		 addKeyListener(this);
 		Frame frame = (Frame) this.getParent().getParent();
 		frame.setTitle("Q-Bot Alpha");
-		super.init();
+		
+		try{
+			base = getDocumentBase();
+		} catch (Exception e) {
+			
+		}
+		character = getImage(base, "data/character.png");
 	}
 
 	@Override
 	public void start() {
+		robot = new Robot();
 		Thread thread = new Thread(this);
-		super.start();
+		thread.start();
 	}
 
 	@Override
@@ -37,6 +57,7 @@ public class StartingClass extends Applet implements Runnable {
 	@Override
 	public void run() {
 		while(true){
+			robot.update();
 			repaint();
 			try{
 				Thread.sleep(17);
@@ -44,6 +65,81 @@ public class StartingClass extends Applet implements Runnable {
 				e.printStackTrace();
 			}
 		}
+		
+	}
+	
+	@Override
+	public void update(Graphics g){
+		if(image ==null){
+			image = createImage(this.getWidth(),this.getHeight());
+			second = image.getGraphics();
+		}
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		paint(second);
+		
+		g.drawImage(image,  0, 0, this);
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		g.drawImage(character, robot.getCenterX() -61, robot.getCenterY() -63, this);
+	}
+	
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			System.out.println("Move up");
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			System.out.println("Move down");
+			break;
+		case KeyEvent.VK_LEFT:
+			robot.moveLeft();
+			break;
+		case KeyEvent.VK_RIGHT:
+			robot.moveRight();
+			break;
+		case KeyEvent.VK_SPACE:
+			System.out.println("Jump");
+			robot.jump();
+			break;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		 switch (e.getKeyCode()) {
+		   case KeyEvent.VK_UP:
+			   System.out.println("Stop moving up");
+		      break;
+
+		   case KeyEvent.VK_DOWN:
+			   System.out.println("Stop moving down");
+		      break;
+
+		   case KeyEvent.VK_LEFT:
+			   robot.stop();
+		      break;
+
+		   case KeyEvent.VK_RIGHT:
+			   robot.stop();
+		      break;
+		   case KeyEvent.VK_SPACE:
+			   System.out.println("Stop jumping");
+		      break;
+
+		   }
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
