5e208f95433339ab2ee282770e00fe5d32292be0
valgard <valgard@bbc>
2014-11-05 23:26:43 +0100

    2.0

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -16,11 +16,16 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
 	private Robot robot;
 	private Heliboy hb, hb2;
-	private Image image, currentSprite, character, character2, character3, characterDown, characterJumped, background, heliboy, heliboy2, heliboy3, heliboy4, heliboy5;
+	private Image image, currentSprite, character, character2, character3, characterDown, characterJumped, 
+	background, heliboy, heliboy2, heliboy3, heliboy4, heliboy5;
+	public static Image tiledirt, tileocean;
+	
 	private Graphics second;
 	private URL base;
 	private static Background bg1, bg2;
 	private Animation anim, hanim;
+	
+	private ArrayList<Tile> tilearray = new ArrayList<Tile>();
 	
 	@Override
 	public void init() {
@@ -50,6 +55,8 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		heliboy3 = getImage(base, "data/heliboy3.png");
 		heliboy4 = getImage(base, "data/heliboy4.png");
 		heliboy5 = getImage(base, "data/heliboy5.png");
+		tiledirt = getImage(base, "data/tiledirt.png");
+		tileocean = getImage(base, "data/tileocean.png");
 		
 		anim = new Animation();
 		anim.addFrame(character, 1250);
@@ -78,6 +85,24 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void start() {
 		bg1 = new Background(0, 0);
 		bg2 = new Background(2160, 0);
+		
+		for (int i = 0; i < 200; i++) {
+			for (int j = 0; j < 12; j++) {
+
+			if (j == 11) {
+			Tile t = new Tile(i, j, 2);
+			tilearray.add(t);
+
+			} if (j == 10) {
+			Tile t = new Tile(i, j, 1);
+			tilearray.add(t);
+
+			}
+			}
+			}
+		
+			
+		
 		hb = new Heliboy(340, 360);
 		hb2 = new Heliboy(700, 360);
 		robot = new Robot();
@@ -115,6 +140,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 			projectiles.remove(i);
 			}
 			}
+			updateTiles();
 			hb.update();
 			hb2.update();
 			bg1.update();
@@ -152,7 +178,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 	public void paint(Graphics g){
 		g.drawImage(background, bg1.getBgX(), bg1.getBgY(), this);
 		g.drawImage(background, bg2.getBgX(), bg2.getBgY(), this);
-		
+		paintTiles(g);
 		
 		ArrayList projectiles = robot.getProjectiles();
 		for (int i = 0; i < projectiles.size(); i++) {
@@ -166,6 +192,23 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 		g.drawImage(hanim.getImage(), hb2.getCenterX() - 48, hb2.getCenterY() - 48, this);
 	}
 	
+	private void updateTiles() {
+
+		for (int i = 0; i < tilearray.size(); i++) {
+		Tile t = (Tile) tilearray.get(i);
+		t.update();
+		}
+
+
+		}
+
+
+		private void paintTiles(Graphics g) {
+		for (int i = 0; i < tilearray.size(); i++) {
+		Tile t = (Tile) tilearray.get(i);
+		g.drawImage(t.getTileImage(), t.getTileX(), t.getTileY(), this);
+		}
+		}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
