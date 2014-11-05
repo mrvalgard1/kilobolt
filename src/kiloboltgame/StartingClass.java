1f4d0ee078512281c37e4f895d5ce1952344bd57
valgard <valgard1@o2.pl>
2014-11-05 12:25:36 +0100

    1

---
+++ src/kiloboltgame/StartingClass.java
@@ -0,0 +1,51 @@
+package kiloboltgame;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+
+public class StartingClass extends Applet implements Runnable {
+
+	@Override
+	public void init() {
+		setSize(800, 480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-Bot Alpha");
+		super.init();
+	}
+
+	@Override
+	public void start() {
+		Thread thread = new Thread(this);
+		super.start();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		while(true){
+			repaint();
+			try{
+				Thread.sleep(17);
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	
+}
